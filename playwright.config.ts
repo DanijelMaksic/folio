9f4add18a1d1082f867a5ceaf6ddba5edbc84b78
DanijelMaksic <danijel.maksic77@gmail.com>
2026-07-03 12:28:23 +0200

    update config to include db url

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,6 +7,9 @@ export default defineConfig({
       url: 'http://localhost:5173',
       reuseExistingServer: !process.env.CI,
       timeout: 120_000,
+      env: {
+         DATABASE_URL: process.env.DATABASE_URL ?? '',
+      },
    },
    use: {
       baseURL: 'http://localhost:5173',
